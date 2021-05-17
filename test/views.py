1f079922b1fd9c41049dd54759beb4fe4378f80f
hyperdream <hyperdream@DESKTOP-8S89PF0.localdomain>
2021-05-17 10:36:20 +0200

    added more views: Details, Login, Register

--- test/views.py
+++ test/views.py
@@ -3,5 +3,18 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. You're at the test index.")
+
+def details(request):
+    # x = 5
+    output = "<h1>Hello world, you're the details view:<h1>"
+    return HttpResponse(output)
+
+def login(request):
+
+    return HttpResponse("Login view")
+
+def register(request):
+    
+    return HttpResponse("Register")
 # Create your views here.
