4d97d31293addb557b27d4f16e7bfb0cd4de6798
hyperdream <hyperdream@DESKTOP-8S89PF0.localdomain>
2021-06-05 11:29:11 +0200

    Test form

--- main/views.py
+++ main/views.py
@@ -12,6 +12,15 @@ def details(request):
     return render(request, 'main/Details.html',{})
 
 def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+
     return render(request, 'main/Login.html',{})
 
 def register(request):
